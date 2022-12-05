a68e848f5425d484e8f0a0c8b967ed7b5b87b3bc
DanieleLee <keojin28@gmail.com>
2022-12-05 11:17:15 +0900

    221205 app of proto end

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,8 +1,38 @@
+import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import { getFilteredEvents } from "../../dummy-data";
+
 function FilteredEventsPage(){
+    const router = useRouter();
+    
+    const filterData = router.query.slug;
+
+    if(!filterData){
+        return <p className="center">Loading......</p>
+    }
+
+    const filteredYear = filterData[0];
+    const filteredMonth = filterData[1];
+
+    const numYear = +filteredYear;
+    const numMonth = + filteredMonth;
+
+    if(isNaN(numYear) || isNaN(numMonth)){
+        return <p>unvalid values...</p>
+    }
+
+    const filteredEvents = getFilteredEvents({
+        year: numYear,
+        month: numMonth
+    });
+
+    if(!filteredEvents || filteredEvents.length === 0){
+        return <p>No events found for the choose</p>
+    }
 
     return(
         <div>
-            <h1>Filtered Events</h1>
+            <EventList items={filteredEvents}/>
         </div>
     );
 }
